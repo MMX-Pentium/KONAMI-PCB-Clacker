3d2e1a2354d9b64a992730c63ed1b47026dd063f
MMX <151848428+MMX-Pentium@users.noreply.github.com>
2023-11-24 11:45:34 +0900

    Update step2.bat

--- step2.bat
+++ step2.bat
@@ -1,6 +1,6 @@
 @echo off
 
-REM 再起動後の本処理、konamiフォルダの移動とUSB書き込み禁止の無効化と自身のスタートアップを削除
+REM 再起動後の本処理、konamiフォルダの移動と自身のスタートアップを削除
 
 REM 対象のディレクトリ内に実行しているプログラムがあるとフォルダの操作ができないため、ディレクトリ内のexeファイルすべてをタスクキル
 cd /d C:\konami
@@ -8,7 +8,12 @@ for %%i in (*.exe) do taskkill /f /im "%%i"
 
 cd C:\
 xcopy C:\konami G:\konami\
-rd /s /q C:\konami
+if exits G:\konami\ (
+ rd /s /q C:\konami
+) else (
+ echo エラー:フォルダの移動に失敗
+ pause
+)
 del /q "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\step1.bat"
 
 explorer
